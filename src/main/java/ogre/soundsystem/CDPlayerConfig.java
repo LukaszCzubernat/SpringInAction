d2b2bceb0e5b40e26a92a524e4e6d27d73074d82
lukasz_czubernat <lukasz_czubernat@epam.com>
2016-09-21 11:21:10 +0200

    Refactor - Change to JavaConfig I've replaced component scan with JavaConfig.

--- src/main/java/ogre/soundsystem/CDPlayerConfig.java
+++ src/main/java/ogre/soundsystem/CDPlayerConfig.java
@@ -1,9 +1,19 @@
 package ogre.soundsystem;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = MediaDevice.class)
 public class CDPlayerConfig {
+
+    @Bean(name = "lonelyHeatsClub")
+    public CompactDisc sgtPeppers(){
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public MediaPlayer cdPlayer(CompactDisc compactDisc){
+        return new CDPlayer(compactDisc);
+    }
+
 }
